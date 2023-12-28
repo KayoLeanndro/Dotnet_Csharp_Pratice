95a8819a450982c9c2f33c69b4a214632fcca01c
kayo.leanndro <kayoleanndro2@gmail.com>
2023-12-28 11:32:37 -0300

    Done! [Project calc]

--- 3_Calculadora/Program.cs
+++ 3_Calculadora/Program.cs
@@ -2,16 +2,15 @@
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.WriteLine("Seja bem vindo a Calculadora!\n");
+Console.WriteLine("Seja bem-vindo à Calculadora!\n");
 
 Calculadora c = new Calculadora();
 string opcao = string.Empty;
 bool exibirMenuCalculadora = true;
-
+int n1, n2;
 
 while (exibirMenuCalculadora)
 {
-    Console.Clear();
     Console.WriteLine("Que conta você deseja realizar? :");
     Console.WriteLine("1 - Somar");
     Console.WriteLine("2 - Subtrair");
@@ -21,40 +20,61 @@ while (exibirMenuCalculadora)
     Console.WriteLine("6 - Potencia");
     Console.WriteLine("0 - Encerrar");
 
-    switch (Console.ReadLine())
+    opcao = Console.ReadLine();
+
+
+    switch (opcao)
     {
         case "1":
-           
+            Console.WriteLine("Digite dois números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado de {n1} + {n2} : {c.Somar(n1, n2)}");
             break;
 
         case "2":
-            
+            Console.WriteLine("Digite dois números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado de {n1} - {n2} : {c.Subtrair(n1, n2)}");
             break;
 
         case "3":
-           
+            Console.WriteLine("Digite dois números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado de {n1} * {n2} : {c.Multiplicar(n1, n2)}");
             break;
 
         case "4":
-           
+            Console.WriteLine("Digite dois números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado de {n1} / {n2} : {c.Dividir(n1, n2)}");
             break;
 
         case "5":
-
+            Console.WriteLine("Digite dois números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resto da divisão de {n1} por {n2} : {c.RestoDivisao(n1, n2)}");
             break;
-        
-        case "6":
 
+        case "6":
+            Console.WriteLine("Digite dois números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado de {n1} elevado à potência {n2} : {c.Potenciacao(n1, n2)}");
+            break;
+
+        case "0":
+            exibirMenuCalculadora = false;
             break;
 
         default:
             Console.WriteLine("Opção inválida");
             break;
     }
-
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
 }
 
 Console.WriteLine("O programa se encerrou");
-
