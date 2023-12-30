4f1920175ca326d9cc34af7c78e334b621d1b706
kayo.leanndro <kayoleanndro2@gmail.com>
2023-12-30 09:01:28 -0300

    Starting  [Project dado row]

--- 5_JogarDados/Program.cs
+++ 5_JogarDados/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _5_JogarDados.models;
+JogoDeDados jgDados = new JogoDeDados();
+Pessoa p1 = new Pessoa("Julia");
+Pessoa p2 = new Pessoa("Marcos");
+
+for(int i = 0; i < 3; i++)
+{
+    if(p1.RodarDados() > p2.RodarDados())
+    {
+        Console.WriteLine($"{p1.Nome} tirou o numero")
+    }
+}
